6eada9429357c84f73f05323a748f8bd128fbea1
oscarcg91 <oscarcrgar@gmail.com>
2021-11-17 21:38:58 -0600

    broken homepage into smaller components and style components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import './homepage.styles.scss'
+import Directory from "../../components/directory/directory.component";
+
+
+const HomePage = () => (
+    <div className="homePage"> 
+        <Directory/>
+    </div>
+)
+
+export default HomePage;
