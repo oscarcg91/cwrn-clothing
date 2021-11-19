b182c3a1ee31891155c8e5384be868dc54230989
oscarcg91 <oscarcrgar@gmail.com>
2021-11-18 22:36:52 -0600

    adding routing to shops page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Routes } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="shop" element={<ShopPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
